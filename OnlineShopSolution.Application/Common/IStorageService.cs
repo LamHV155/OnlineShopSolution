36aceb153ff6d9b8ceee67486b149d897a9e8217
lamhv155@gmail.com <lamhv155@gmail.com>
2021-07-06 16:16:44 +0700

    create storage service

---
+++ OnlineShopSolution.Application/Common/IStorageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopSolution.Service.Common
+{
+    public interface IStorageService
+    {
+        string GetFileUrl(string fileName);
+        Task SaveFileAsync(Stream mediabinaryStream, string fileName);
+
+        Task DeleteFileAsync(string fileName);
+    }
+}
