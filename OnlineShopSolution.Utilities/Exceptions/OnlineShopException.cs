4298690e48cd3247463544c3fca0a6fd02f89f0a
lamhv155@gmail.com <lamhv155@gmail.com>
2021-07-06 16:19:38 +0700

    Add exception

---
+++ OnlineShopSolution.Utilities/Exceptions/OnlineShopException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShopSolution.Utilities.Exceptions
+{
+    public class OnlineShopException : Exception
+    {
+        public OnlineShopException() { }
+
+        public OnlineShopException(string msg) : base(msg) { }
+
+        public OnlineShopException(string msg, Exception inner): base(msg, inner) { }
+    }
+}
