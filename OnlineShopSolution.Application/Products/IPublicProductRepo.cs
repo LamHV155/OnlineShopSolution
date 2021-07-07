cf63661550874befdfdbaf35800f36f455b88156
lamhv155@gmail.com <lamhv155@gmail.com>
2021-07-07 10:01:49 +0700

    create api project

--- OnlineShopSolution.Application/Products/IPublicProductRepo.cs
+++ OnlineShopSolution.Application/Products/IPublicProductRepo.cs
@@ -6,10 +6,11 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnlineShopSolution.Service.Repositories.Products
+namespace OnlineShopSolution.Service.Products
 {
     public interface IPublicProductRepo
     {
         public Task<PagedResult<ProductViewModel>> GetAllByCategoryId(pub_GetProductPagingRequest req);
+        public Task<List<ProductViewModel>> GetAll_test();
     }
 }
